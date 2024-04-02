2606ddb62d38a01f0185d7f11c585849e008c462
xscxsc <849005527@qq.com>
2024-04-02 13:09:44 +0800

    create

---
+++ welcome/WelcomeActivity.java
@@ -0,0 +1,16 @@
+package com.arcsoft.App.welcome;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.arcsoft.arcfacedemo.R;
+
+public class WelcomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_welcome);
+    }
+}
